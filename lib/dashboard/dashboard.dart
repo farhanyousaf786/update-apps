91300a6a45e7d3350b346bd84864dde4a4cc1844
Farhan Yousaf <farhanyousaf1996@gmail.com>
2022-12-27 11:58:34 -0500

    UI setup

--- lib/dashboard/dashboard.dart
+++ lib/dashboard/dashboard.dart
@@ -1,3 +1,4 @@
+import 'package:facebook_audience_network/facebook_audience_network.dart';
 import 'package:url_launcher/url_launcher.dart';
 import '../pages/AppsListScreen.dart';
 import '../pages/SystemAppsListScreen.dart';
@@ -14,6 +15,79 @@ class DashBoard extends StatefulWidget {
 }
 
 class _DashBoardState extends State<DashBoard> {
+  @override
+  void initState() {
+    FacebookAudienceNetwork.init();
+    _loadInterstitialAd();
+    _showNativeAd();
+    // Future.delayed(const Duration(seconds: 30), () {
+    //   _showInterstitialAd();
+    //
+    // });
+
+    super.initState();
+  }
+
+  bool _isInterstitialAdLoaded = false;
+
+  void _loadInterstitialAd() {
+    FacebookInterstitialAd.loadInterstitialAd(
+      placementId: "1336093853816192_1336095053816072",
+      listener: (result, value) {
+        print(">> FAN > Interstitial Ad: $result --> $value");
+        if (result == InterstitialAdResult.LOADED)
+          _isInterstitialAdLoaded = true;
+
+        /// Once an Interstitial Ad has been dismissed and becomes invalidated,
+        /// load a fresh Ad by calling this function.
+        if (result == InterstitialAdResult.DISMISSED &&
+            value["invalidated"] == true) {
+          _isInterstitialAdLoaded = false;
+          _loadInterstitialAd();
+        }
+      },
+    );
+  }
+
+  _showInterstitialAd() {
+    if (_isInterstitialAdLoaded == true)
+      FacebookInterstitialAd.showInterstitialAd();
+    else
+      print("Interstial Ad not yet loaded!");
+  }
+
+  Widget _currentAd = SizedBox(
+    width: 0.0,
+    height: 0.0,
+  );
+
+  _showNativeAd() {
+    setState(() {
+      _currentAd = _nativeAd();
+    });
+  }
+
+  Widget _nativeAd() {
+    return FacebookNativeAd(
+      //1316724952486390_1316726822486203
+      placementId: "1336093853816192_1336094377149473",
+      adType: NativeAdType.NATIVE_AD_VERTICAL,
+      width: double.infinity,
+      height: 300,
+      backgroundColor: Colors.blue,
+      titleColor: Colors.white,
+      descriptionColor: Colors.white,
+      buttonColor: Colors.deepPurple,
+      buttonTitleColor: Colors.white,
+      buttonBorderColor: Colors.white,
+      listener: (result, value) {
+        print("Native Ad: $result --> $value");
+      },
+      keepExpandedWhileLoading: true,
+      expandAnimationDuraion: 1000,
+    );
+  }
+
   /// This function will lead us to browser to run a url.
   _launchURL(String url) async {
     if (await canLaunch(url)) {
@@ -40,11 +114,10 @@ class _DashBoardState extends State<DashBoard> {
           title: const Text(
             "App Tools",
             style: TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.w700,
-              fontSize: 20,
-              fontFamily: 'bal'
-            ),
+                color: Colors.white,
+                fontWeight: FontWeight.w700,
+                fontSize: 20,
+                fontFamily: 'bal'),
           ),
           actions: [
             GestureDetector(
@@ -71,74 +144,84 @@ class _DashBoardState extends State<DashBoard> {
           child: Center(
             child: Padding(
               padding: const EdgeInsets.only(left: 10, right: 10, top: 12),
-              child: GridView.count(
-                  primary: false,
-                  padding: const EdgeInsets.all(15),
-                  crossAxisSpacing: 30,
-                  mainAxisSpacing: 15,
-                  crossAxisCount: 2,
-                  children: <Widget>[
-                    TextButton(
 
-                      onPressed: () {
-                        Navigator.of(context).push(MaterialPageRoute(
-                            builder: (context) => AppsListScreen()));
-                      },
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
 
-                      child: Material(
-                        elevation: 8,
-                        borderRadius: BorderRadius.circular(15),
-                        child: Center(
-                            child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Image.asset(
-                              'assets/images/apps.png',
-                              width: 40,
-                              height: 40,
-                            ),
-                            const SizedBox(height: 10),
-                            Text(
-                              "User Apps",
-                              style: TextStyle(
-                                  color: Colors.blue.shade700,
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.w700,
-                                  fontFamily: 'bal'),                            ),
-                          ],
-                        )),
+
+                    Row(children: [
+                      Container(
+                      height: 120,
+                      width: MediaQuery.of(context).size.width/2.5,
+                      child: TextButton(
+                        onPressed: () {
+                          Navigator.of(context).push(MaterialPageRoute(
+                              builder: (context) => AppsListScreen()));
+                        },
+                        child: Material(
+                          elevation: 8,
+                          borderRadius: BorderRadius.circular(15),
+                          child: Center(
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Image.asset(
+                                    'assets/images/apps.png',
+                                    width: 40,
+                                    height: 40,
+                                  ),
+                                  const SizedBox(height: 10),
+                                  Text(
+                                    "User Apps",
+                                    style: TextStyle(
+                                        color: Colors.blue.shade700,
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.w700,
+                                        fontFamily: 'bal'),
+                                  ),
+                                ],
+                              )),
+                        ),
                       ),
                     ),
-                    TextButton(
-                      onPressed: () {
-                        Navigator.of(context).push(MaterialPageRoute(
-                            builder: (context) => SystemAppsListScreen()));
-                      },
-                      child: Material(
-                        elevation: 8,
-                        borderRadius: BorderRadius.circular(15),
-                        child: Center(
-                            child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Image.asset(
-                              'assets/images/system_apps.png',
-                              width: 40,
-                              height: 40,
-                            ),
-                            const SizedBox(height: 10),
-                            Text(
-                              "System Apps",
-                              style: TextStyle(
-                                  color: Colors.blue.shade700,
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.w700,
-                                  fontFamily: 'bal'),
-                            ),
-                          ],
-                        )),
-                      ),
-                    ),
+                      Container(
+                        height: 120,
+                        width: MediaQuery.of(context).size.width/2.5,
+                        child: TextButton(
+                          onPressed: () {
+                            Navigator.of(context).push(MaterialPageRoute(
+                                builder: (context) => SystemAppsListScreen()));
+                          },
+                          child: Material(
+                            elevation: 8,
+                            borderRadius: BorderRadius.circular(15),
+                            child: Center(
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Image.asset(
+                                      'assets/images/system_apps.png',
+                                      width: 40,
+                                      height: 40,
+                                    ),
+                                    const SizedBox(height: 10),
+                                    Text(
+                                      "System Apps",
+                                      style: TextStyle(
+                                          color: Colors.blue.shade700,
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.w700,
+                                          fontFamily: 'bal'),
+                                    ),
+                                  ],
+                                )),
+                          ),
+                        ),
+                      ),],),
+
+
+
                     TextButton(
                       onPressed: () {
                         Navigator.of(context).push(MaterialPageRoute(
@@ -149,23 +232,24 @@ class _DashBoardState extends State<DashBoard> {
                         borderRadius: BorderRadius.circular(15),
                         child: Center(
                             child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Image.asset(
-                              'assets/images/delete_app.png',
-                              width: 40,
-                              height: 40,
-                            ),
-                            const SizedBox(height: 10),
-                            Text(
-                              "Un-install App",
-                              style: TextStyle(
-                                  color: Colors.blue.shade700,
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.w700,
-                                  fontFamily: 'bal'),                            ),
-                          ],
-                        )),
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Image.asset(
+                                  'assets/images/delete_app.png',
+                                  width: 40,
+                                  height: 40,
+                                ),
+                                const SizedBox(height: 10),
+                                Text(
+                                  "Un-install App",
+                                  style: TextStyle(
+                                      color: Colors.blue.shade700,
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w700,
+                                      fontFamily: 'bal'),
+                                ),
+                              ],
+                            )),
                       ),
                     ),
                     TextButton(
@@ -177,23 +261,24 @@ class _DashBoardState extends State<DashBoard> {
                         borderRadius: BorderRadius.circular(15),
                         child: Center(
                             child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Image.asset(
-                              'assets/images/system_update.png',
-                              width: 40,
-                              height: 40.0,
-                            ),
-                            const SizedBox(height: 10),
-                            Text(
-                              "System Update",
-                              style: TextStyle(
-                                  color: Colors.blue.shade700,
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.w700,
-                                  fontFamily: 'bal'),                            ),
-                          ],
-                        )),
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Image.asset(
+                                  'assets/images/system_update.png',
+                                  width: 40,
+                                  height: 40.0,
+                                ),
+                                const SizedBox(height: 10),
+                                Text(
+                                  "System Update",
+                                  style: TextStyle(
+                                      color: Colors.blue.shade700,
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w700,
+                                      fontFamily: 'bal'),
+                                ),
+                              ],
+                            )),
                       ),
                     ),
                     TextButton(
@@ -206,24 +291,24 @@ class _DashBoardState extends State<DashBoard> {
                         borderRadius: BorderRadius.circular(20),
                         child: Center(
                             child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Image.asset(
-                              'assets/images/cpu.png',
-                              width: 40,
-                              height: 40,
-                            ),
-                            const SizedBox(height: 10),
-                            Text(
-                              "Processor",
-                              style: TextStyle(
-                                  color: Colors.blue.shade700,
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.w700,
-                                  fontFamily: 'bal'),
-                            ),
-                          ],
-                        )),
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Image.asset(
+                                  'assets/images/cpu.png',
+                                  width: 40,
+                                  height: 40,
+                                ),
+                                const SizedBox(height: 10),
+                                Text(
+                                  "Processor",
+                                  style: TextStyle(
+                                      color: Colors.blue.shade700,
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w700,
+                                      fontFamily: 'bal'),
+                                ),
+                              ],
+                            )),
                       ),
                     ),
                     TextButton(
@@ -236,31 +321,42 @@ class _DashBoardState extends State<DashBoard> {
                         borderRadius: BorderRadius.circular(15),
                         child: Center(
                             child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Image.asset(
-                              'assets/images/storage.png',
-                              width: 40,
-                              height: 40,
-                            ),
-                            const SizedBox(height: 10),
-                            Text(
-                              "To-Do App",
-                              style: TextStyle(
-                                  color: Colors.blue.shade700,
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.w700,
-                                  fontFamily: 'bal'),
-                            ),
-                          ],
-                        )),
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Image.asset(
+                                  'assets/images/storage.png',
+                                  width: 40,
+                                  height: 40,
+                                ),
+                                const SizedBox(height: 10),
+                                Text(
+                                  "To-Do App",
+                                  style: TextStyle(
+                                      color: Colors.blue.shade700,
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w700,
+                                      fontFamily: 'bal'),
+                                ),
+                              ],
+                            )),
                       ),
                     ),
-                  ]),
+                    SizedBox(height: 300,)
+
+                  ],
+                ),
+              )
             ),
           ),
         ),
       ),
+      Padding(
+        padding: const EdgeInsets.only(top: 2),
+        child: Align(
+          alignment: Alignment(0, 1.0),
+          child: _currentAd,
+        ),
+      )
     ]);
   }
 }
